0d7fc68dd7900218347a6e494bb2cb28e3156cfd
Guido <g.j.schetters@gmail.com>
2015-01-18 21:46:15 +0100

    ewfioj

--- moisture.py
+++ moisture.py
@@ -4,6 +4,7 @@ from mst_sensor import Sensor
 from rgb import RGB 
 
 debug = True
+delay = 1
 
 rgb = RGB(23,24,25,debug)
 sensor = Sensor(0, debug)
@@ -11,7 +12,7 @@ sensor = Sensor(0, debug)
 while True:
 
         # Read the sensor data
-        sensor_level = sensor.ReadChannel(sensor_channel)
+        sensor_level = sensor.ReadChannel()
         sensor_volts = sensor.ConvertVolts(sensor_level,2)
 
         # Print out results
