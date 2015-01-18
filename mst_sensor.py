e60d72b6b943eb922e188bfecf07cc1edc81fc7c
Guido <g.j.schetters@gmail.com>
2015-01-18 21:58:51 +0100

    +"self."

--- mst_sensor.py
+++ mst_sensor.py
@@ -8,15 +8,13 @@ spi = spidev.SpiDev()
 spi.open(0,0)
 
 class Sensor(object):
-	
-
 	def __init__(self,channel,debug):
 		self.channel = channel
 		self.debug = debug
 		
 
 	def ReadChannel(channel):
-		adc = spi.xfer2([1,(8+channel)<<4,0])
+		adc = spi.xfer2([1,(8+self.channel)<<4,0])
 		data = ((adc[1]&3) << 8) + adc[2]
 		return data
 
