f88f44f76e1c41e3beead6fb5b5ea64b9edd3caa
Guido <g.j.schetters@gmail.com>
2015-01-18 22:25:19 +0100

    typos

--- moisture.py
+++ moisture.py
@@ -10,9 +10,9 @@ rgb = RGB(23,24,25,debug)
 sensor = Sensor(0, debug)
 
 if debug:
-	rgb.blink(blue,0.1,1)
+	rgb.blink("Blue",0.1,1)
 else:
-	rgb.blink(green,0.1,1)
+	rgb.blink("Green",0.1,1)
 
 while True:
 
