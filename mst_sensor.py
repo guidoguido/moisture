0faac763c4883ef43c199db3322eca583f5f9b4d
Guido <g.j.schetters@gmail.com>
2015-01-18 21:40:03 +0100

    more typos

--- mst_sensor.py
+++ mst_sensor.py
@@ -15,10 +15,10 @@ class Sensor(object):
 
 	def ReadChannel(channel):
 		adc = spi.xfer2([1,(8+channel)<<4,0])
-    data = ((adc[1]&3) << 8) + adc[2]
-    return data
+		data = ((adc[1]&3) << 8) + adc[2]
+		return data
 
-  def ConvertVolts(data,places):
-  	volts = (data * 3.3) / float(1023)
-    volts = round(volts,places)
-    return volts
+	def ConvertVolts(data,places):
+		volts = (data * 3.3) / float(1023)
+		volts = round(volts,places)
+		return volts
