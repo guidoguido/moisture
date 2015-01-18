5a3565a238408a581dfeb9f221fd8c4826717dc9
Guido <g.j.schetters@gmail.com>
2015-01-18 21:35:31 +0100

    rebuilding to classes

---
+++ moisture.py
@@ -0,0 +1,25 @@
+# main
+import time
+import Sensor from mst_sensor.py
+import RGB from rgb.py
+
+debug = True
+
+rgb = RGB(23,24,25,debug)
+sensor = Sensor(0, debug)
+
+while True:
+
+        # Read the sensor data
+        sensor_level = ReadChannel(sensor_channel)
+        sensor_volts = ConvertVolts(sensor_level,2)
+
+        # Print out results
+        print"---"
+        if debug:
+                print("Data: {}".format(sensor_level))
+        else:
+                print("Data: {} ({}V)".format(sensor_level,sensor_volts))
+
+        # Wait before repeating loop
+        time.sleep(delay)
---
+++ mst_sensor.py
@@ -0,0 +1,24 @@
+#class version of read_sensor.py
+class Sensor(object):
+
+	import time
+	import os
+	import spidev
+
+	# Open SPI bus
+	spi = spidev.SpiDev()
+	spi.open(0,0)
+
+	def __init__(self, channel, debug):
+		self.channel = channel
+		self.debug = debug
+
+	def ReadChannel(channel):
+		adc = spi.xfer2([1,(8+channel)<<4,0])
+    data = ((adc[1]&3) << 8) + adc[2]
+    return data
+
+  def ConvertVolts(data,places):
+  	volts = (data * 3.3) / float(1023)
+    volts = round(volts,places)
+    return volts
