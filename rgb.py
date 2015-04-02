57c7934cb0e5ba3b1c4665a96b907cb982426341
Guido <g.j.schetters@gmail.com>
2015-04-02 21:49:23 +0200

    sw

--- rgb.py
+++ rgb.py
@@ -22,7 +22,7 @@ class RGB(threading.Thread):
 	
 	def run(self):
 		while True:
-			print threading.Thread.main_thread()
+			print threading.Thread
 			pass
 			GPIO.output(self.blue, True)
 			print "On"
