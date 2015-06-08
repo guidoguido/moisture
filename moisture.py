0f3f3ab3507685342a00d30b3a3e0fc63d857886
Guido <g.j.schetters@gmail.com>
2015-06-08 16:32:39 +0200

    revised blink() measure() added timer()

--- moisture.py
+++ moisture.py
@@ -12,22 +12,49 @@ error = False
 sensor = Sensor(0, debug)
 led = RGB(23,24,25)
 
-#fucntion to show in which mode is running or if there is a problem
-def status_indicator():
-	prev_milli_time = int(round(time.time()*1000))
-	interval = 0
+timer()
+
+def blink_status(): #fucntion to show in which mode is running or if there is a problem
 	led_on = False
+	if error:
+		color = "Red"
+	elif debug:
+		color = "Blue"
+	else:
+		color = "Green"
+
+	if led_on:
+		# interval = 4000	#interval between blinks
+		led.off()
+		led_on = False
+	else:
+		# interval = 100	#blink durration
+		led.on(color)
+		led_on = True
+
+def measure(): #function to let all the sensors(1) measure 
+	# Read the sensor data
+	sensor_level = sensor.ReadChannel()
+	sensor_volts = sensor.ConvertVolts(sensor_level,2)
+
+	# Print out results
+	print"---"
+	if not debug:
+		print("Data: {}".format(sensor_level))
+	else:
+		print("Data: {} ({}V)".format(sensor_level,sensor_volts))
+
+def timer(): #function to time multiple events
+	prev_milli_time_1 = int(round(time.time()*1000))
+	prev_milli_time_2 = int(round(time.time()*1000))
+	blink_interval = 1000
+	measure_interval = 5000
 	try:
 		while True:
-			if error:
-				color = "Red"
-			elif debug:
-				color = "Blue"
-			else:
-				color = "Green"
+			if current_milli_time() - prev_milli_time_1 > blink_interval:	#Timer 
+				prev_milli_time_1 = int(current_milli_time())
+				blink_status()
 
-			if current_milli_time() - prev_milli_time > interval:	#Timer 
-				prev_milli_time = int(current_milli_time())					#prev time is updated
 				if led_on:
 					interval = 4000	#interval between blinks
 					led.off()
@@ -36,29 +63,14 @@ def status_indicator():
 					interval = 100	#blink durration
 					led.on(color)
 					led_on = True
-	except (KeyboardInterrupt, SystemExit):
- 		GPIO.cleanup()
 
-def measure(): #function to let all the sensors(1) measure 
-	prev_milli_time = int(round(time.time()*1000))
-	interval = 3
-	try:
-		while True:
-			if current_milli_time() - prev_milli_time > interval:	#Timer 
-				prev_milli_time = int(current_milli_time())
+			if current_milli_time() - prev_milli_time_2 > measure_interval:	#Timer 
+				prev_milli_time_2 = int(current_milli_time())
+				measure()
 
-				# Read the sensor data
-				sensor_level = sensor.ReadChannel()
-				sensor_volts = sensor.ConvertVolts(sensor_level,2)
-
-				# Print out results
-				print"---"
-				if not debug:
-					print("Data: {}".format(sensor_level))
-				else:
-					print("Data: {} ({}V)".format(sensor_level,sensor_volts))
-	except (KeyboardInterrupt, SystemExit):
+	except Exception, e:
+		raise
+	else:
+		pass
+	finally:
 		GPIO.cleanup()
-
-status_indicator()
-measure()
